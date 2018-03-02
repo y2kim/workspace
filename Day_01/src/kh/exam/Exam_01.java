d052e4ab1e6d6c8148a7c81d31fdb93a68cd9075
y2kim <kchy0806@naver.com>
2018-03-02 09:33:33 +0900

    Initial commit

---
+++ Day_01/src/kh/exam/Exam_01.java
@@ -0,0 +1,27 @@
+package kh.exam;
+// import java.lang.*; // java_lang 파일  가져오겠다는 뜻
+/*
+ * BSD style :조직체  
+ * 				   {
+ * 		System.out.println("Hello world");
+ * 				   }
+ * GNU style :조직체 들여쓰기
+ * 		   {
+ * 		   System.out.println("Hello world");
+ * 		}
+ * K&R style : 조직체 ↓ 이 방식 
+ * ; : 문법구문자 
+ * ctrl+I : 자동정렬
+ * */
+public class Exam_01 { // 파일이름
+	// {} : 범위지정자  -코드 범위- 관례 같은거
+	public static void main(String[] arg){ // 초기 실행위치  프로그램 시작지점
+		
+		System.out.println("Hello world");
+		//System.out.println : 명령출력      () : 내용
+		System.out.println(3.14);
+		System.out.println('A');
+		System.out.println(10);
+		
+	}
+}
---
+++ Day_02/src/other/Test.java
@@ -0,0 +1,15 @@
+package other;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+class packed{
+	
+}
+
+
