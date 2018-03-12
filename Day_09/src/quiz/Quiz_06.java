1e8005cea3b26ff734e9119a78be36abe392ab94
y2kim <kchy0806@naver.com>
2018-03-12 17:50:30 +0900

    up
    up

--- Day_09/src/quiz/Quiz_06.java
+++ Day_09/src/quiz/Quiz_06.java
@@ -7,8 +7,8 @@ public class Quiz_06 {
 		int[] im = new int[100];
 
 		for(int i=1;i<101;i++) {
-			char[] arr = String.valueOf(i).toCharArray(); // 이 개념을 몰랐던듯
-			int count =0;
+			char[] arr = String.valueOf(i).toCharArray(); // 이 개념을 몰랐던듯  스트링 배열로 바꾸고 캐릭터 어레이로 바꿈  숫자->문자열->배열
+			int count =0; //3,6,9갯수 포함된 것들
 			for(int j=0;j<arr.length;j++) {
 				if(arr[j]=='3'||arr[j]=='6'||arr[j]=='9') {
 					count ++;
